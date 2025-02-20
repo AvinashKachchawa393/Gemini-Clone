51d8cbd815416dc84bd7683f4483846889c04e07
avinashkachchawa <avinashsaini393@gmail.com>
2025-02-20 17:36:57 +0530

    Fix missing file issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import Main from './components/Main/Main.jsx';
+import Main from './components/Main/Main';
 import Sidebar from './Components/Sidebar/Sidebar.jsx'
 function App() {
   
